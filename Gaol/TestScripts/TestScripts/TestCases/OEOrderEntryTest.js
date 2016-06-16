4e9b655332b268647e9079226f84f3d11edf4723
kian <kian.davoudirad@sage.com>
2016-06-16 14:06:13 -0700

    added Sage Rohit Idea

---
+++ Gaol/TestScripts/TestScripts/TestCases/OEOrderEntryTest.js
@@ -0,0 +1,22 @@
+﻿/* Copyright (c) 1994-2016 Sage Software, Inc.  All rights reserved. */
+
+/// <reference path="../TestUtils/OEOrderEntryTestUtils.js" />
+/// <reference path="../../../OE/Scripts/OrderEntry/Sage.CA.SBS.ERP.Sage300.OE.OrderEntryBehaviour.js" />
+
+
+describe("OE Order Entry tests", function () {
+    //var spyEvent;
+
+    beforeEach(function () {
+        
+    });
+
+    
+    it("Test_OEOrderEntryTest", function () {
+        orderEntryUI.quickPreAuth();
+        //spyEvent = spyOnEvent('#btnQuickPreAuth1', 'click');
+        //$('#btnQuickPreAuth1').trigger("click");
+        //$('#btnQuickPreAuth').trigger("click");
+        expect(1==1).toEqual(true);
+    })
+});
---
+++ Gaol/TestScripts/TestScripts/TestUtils/OEOrderEntryTestUtils.js
@@ -0,0 +1,28 @@
+﻿////////////my changes
+///Columbus-Web/Sage.CA.SBS.ERP.Sage300.Web/Scripts/TestUtil/GlobalTestUtils.js; renamed ajax
+var orderEntryResources = orderEntryResources || {}; //bit unknown where this gets declared. Most likely dynamically generated from c#
+
+//orderEntryResources.FinderTitle = "";
+//orderEntryResources.OrderNumber = "";
+
+var createOrderGridColumns = createOrderGridColumns || {}; //bit unknown; likely kendo generates this on the fly
+var commentsInstructionGridConfigHeader = commentsInstructionGridConfigHeader || {}; //unknown
+var orderShipmentGridConfigHeader = orderShipmentGridConfigHeader || {}; //unknown
+var orderDetailGridColumns = orderDetailGridColumns || {}; //kendo generates this on the fly
+
+//created quickPreAuth ln 1842
+////////////////////////////
+
+var mainScreen = "<div id='orderEntryTabStrip'></div>" +
+    "<button id='btnQuickPreAuth1' type='button' onclick='console.log(\"qqx\");'></button>" +
+    "<button id='btnQuickPreAuth' type='button'></button>" +
+    "<div id='OrderDetailGrid'></div>";
+
+$(document).ready(function () {
+    console.log('append body');
+    $(mainScreen).appendTo("body");
+});
+
+var abc = $(function () {
+    console.log('unit testing..' + $('body').html());
+});
